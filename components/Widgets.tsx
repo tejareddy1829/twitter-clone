2c1b69dbe8b7bf2a991bd27f8f23fbe7f7c6651d
tejareddy1829 <tejareddy292015@gmail.com>
2023-02-15 16:07:39 +0530

    grid

---
+++ components/Widgets.tsx
@@ -0,0 +1,27 @@
+import { SearchIcon } from '@heroicons/react/outline';
+import React from 'react';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
+function Widgets() {
+  return (
+    <div className='mt-2 px-2'>
+      {/* search */}
+      <div className='flex items-center space-x-2 bg-gray-100 p-3 mt-2 rounded-full'>
+        <SearchIcon className='h-5 w-5 text-gray-400' />
+        <input
+          type='text'
+          placeholder='Search twitter'
+          className='flex-1 outline-none bg-transparent'
+        />
+      </div>
+
+      <TwitterTimelineEmbed
+        sourceType='profile'
+        screenName='saitejabevara'
+        options={{ height: 1000 }}
+      />
+    </div>
+  );
+}
+
+export default Widgets;
