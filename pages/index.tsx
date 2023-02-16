7f1ac8764c53f88d17de0f1fcd390666122526d4
tejareddy1829 <tejareddy292015@gmail.com>
2023-02-16 12:05:40 +0530

    grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import Widgets from '../components/Widgets';
 
 const Home: NextPage = () => {
   return (
-    <div className=''>
+    <div className='mx-auto lg:max-w-6xl max-h-screen overflow-hidden'>
       <Head>
         <title>Twitter 2.0</title>
         <link rel='icon' href='/favicon.ico' />
